8fc53e0dbefc9eab5adfaf13d3d355d935afeb0b
kathryntsai <katctsai@gmail.com>
2015-08-31 13:30:21 -0400

    updated calendar url

--- calendar.php
+++ calendar.php
@@ -1,8 +1,10 @@
 <?php
 	
-	// Set the url of the calendar feed.
-	$url = 'https://p01-calendarws.icloud.com/ca/subscribe/1/n6x7Farxpt7m9S8bHg1TGArSj7J6kanm_2KEoJPL5YIAk3y70FpRo4GyWwO-6QfHSY5mXtHcRGVxYZUf7U3HPDOTG5x0qYnno1Zr_VuKH2M';
-
+	// Set the ical url of the calendar feed by changing webcal to https.
+	//$url = 'https://p01-calendarws.icloud.com/ca/subscribe/1/n6x7Farxpt7m9S8bHg1TGArSj7J6kanm_2KEoJPL5YIAk3y70FpRo4GyWwO-6QfHSY5mXtHcRGVxYZUf7U3HPDOTG5x0qYnno1Zr_VuKH2M';
+	$url = 'https://p19-calendars.icloud.com/published/2/44ROgzhahcsqZpKXUZVOKPg2EXIMFnbml5q790athQk0wmzn2QYwlcENvin0AgWjdS6RpcSBPbdg-StC47OQkJC01csEMJ0m7VVeteEDyhU';
+	//$url = 'https://www.google.com/calendar/ical/mtps.us_6k3adsjlsnanqkgk714ik9253g%40group.calendar.google.com/public/basic.ics';
+	
 	/*****************************************/
 
 	// Run the helper function with the desired URL and echo the contents.
